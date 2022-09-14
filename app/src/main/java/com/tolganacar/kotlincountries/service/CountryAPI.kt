40dc3ce2f16dd3beb1adeede719bf836c1e9a86f
Tolga Nacar <83028055+tolganacar@users.noreply.github.com>
2022-09-14 14:46:58 +0300

    Update CountryAPI.kt

--- app/src/main/java/com/tolganacar/kotlincountries/service/CountryAPI.kt
+++ app/src/main/java/com/tolganacar/kotlincountries/service/CountryAPI.kt
@@ -6,8 +6,6 @@ import retrofit2.http.GET
 
 interface CountryAPI {
 
-    //https://raw.githubusercontent.com/atilsamancioglu/IA19-DataSetCountries/master/countrydataset.json
-
     @GET("atilsamancioglu/IA19-DataSetCountries/master/countrydataset.json")
     fun getCountries():Single<List<Country>>
 }
